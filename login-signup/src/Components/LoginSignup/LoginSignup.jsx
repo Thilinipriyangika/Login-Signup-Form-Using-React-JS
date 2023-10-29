814b6f075104de0856be8703796b2f816668e9df
Thilini Priyangika <85427990+Thilinipriyangika@users.noreply.github.com>
2023-10-29 11:59:01 +0530

    components

---
+++ login-signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './LoginSignup.css'
+
+import mail from '../Assets/mail.png'
+import padlock from '../Assets/padlock.png'
+import user from '../Assets/user.png'
+
+export default function LoginSignup() {
+  return (
+    <div className='container'>
+        <div className="header">
+            <div className="text">Sign Up</div>
+            <div className="underline"></div>
+        </div>
+        
+        </div>
+  )
+}
